d2e8a2f65303707bb13bb6f3090c36105fcb17a5
Deivids Dukalskis <deivids.dukalskis@accenture.com>
2023-05-16 19:13:32 +0300

    Added styling.

---
+++ src/Script.js
@@ -0,0 +1,15 @@
+
+function purchase() {
+    document.getElementById("purchaseSuccessful")
+        .innerHTML = "Thank you for purchase!";
+}
+
+function addDrumToCart() {
+    document.getElementById("drumAddedSuccessfully")
+    .innerHTML = "Item added successfully!";
+}
+
+function addGuitarToCart() {
+    document.getElementById("guitarAddedSuccessfully")
+    .innerHTML = "Item added successfully!";
+}
